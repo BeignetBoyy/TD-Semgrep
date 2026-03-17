7519e1c431e95b147d473c2f7031afd833e0566f
Sébastien Lallement <sebastien.lallement@etu.unice.fr>
2026-03-17 11:56:23 +0100

    fix vulnerabitlés

--- 1/XmlReader_Tests.cs
+++ 1/XmlReader_Tests.cs
@@ -16,18 +16,22 @@ namespace XXEExamples.Tests
             AssertXXE.IsXMLParserSafe((string xml) =>
             {
                 XmlReaderSettings settings = new XmlReaderSettings();
-                settings.DtdProcessing = DtdProcessing.Parse;
-                settings.XmlResolver = new XmlUrlResolver();
+                settings.DtdProcessing = DtdProcessing.Prohibit;
+                settings.XmlResolver = null;
                 settings.MaxCharactersFromEntities = 6000;
 
+ 
                 using (MemoryStream stream = new MemoryStream(Encoding.UTF8.GetBytes(xml)))
                 {
-                    XmlReader reader = XmlReader.Create(stream, settings);
-
-                    var xmlDocument = new XmlDocument();
-                    xmlDocument.XmlResolver = new XmlUrlResolver();
-                    xmlDocument.Load(reader);
-                    return xmlDocument.InnerText;
+                    using (XmlReader reader = XmlReader.Create(stream, settings))
+                    {
+                        var xmlDocument = new XmlDocument
+                        {
+                            XmlResolver = null
+                        };
+                        xmlDocument.Load(reader);
+                        return xmlDocument.InnerText;
+                    }
                 }
             }, false);
         }
